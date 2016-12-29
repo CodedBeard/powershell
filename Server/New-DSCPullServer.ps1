2d117f8a846472382e1cfe4b4717473f8493f2e2
Steve Croxford <steve.croxford@biffa.co.uk>
2016-12-29 15:22:15 +0000

    add example dsc config for dsc pull server to use in inital testing

---
+++ Server/New-DSCPullServer.ps1
@@ -0,0 +1,29 @@
+# https://github.com/PowerShell/Whitepapers/blob/master/PullServerCPIG/PullServerCPIG.md
+# This is a very basic Configuration to deploy a pull server instance in a lab environment on Windows Server 2012.
+
+Configuration PullServer {
+Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+        # Load the Windows Server DSC Service feature
+        WindowsFeature DSCServiceFeature
+        {
+          Ensure = 'Present'
+          Name = 'DSC-Service'
+        }
+
+        # Use the DSC Resource to simplify deployment of the web service
+        xDSCWebService PSDSCPullServer
+        {
+          Ensure = 'Present'
+          EndpointName = 'PSDSCPullServer'
+          Port = 8080
+          PhysicalPath = "$env:SYSTEMDRIVE\inetpub\wwwroot\PSDSCPullServer"
+          CertificateThumbPrint = 'AllowUnencryptedTraffic'
+          ModulePath = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
+          ConfigurationPath = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"
+          State = 'Started'
+          DependsOn = '[WindowsFeature]DSCServiceFeature'
+        }
+}
+PullServer -OutputPath 'C:\PullServerConfig\'
+Start-DscConfiguration -Wait -Force -Verbose -Path 'C:\PullServerConfig\'
