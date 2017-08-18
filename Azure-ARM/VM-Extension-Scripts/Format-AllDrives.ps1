c46d7b983fd96427b9ce413b75951e28243dc288
Steve Croxford <s_croxford@hotmail.com>
2017-08-18 19:50:43 +0100

    fix not finding any drives

--- Azure-ARM/VM-Extension-Scripts/Format-AllDrives.ps1
+++ Azure-ARM/VM-Extension-Scripts/Format-AllDrives.ps1
@@ -1,8 +1,13 @@
 $i = 0;
-(get-disk).where({$_.OperationalStatus -eq "Offline"}) |% {
+(get-disk).where({$_.Number -notin "0","1"}) |% {
     $d = $_;
+    if($d.PartitionStyle -ne "RAW"){
+        continue;
+    }
     Initialize-Disk -Number $($d.Number) -PartitionStyle GPT;
     $dp = New-Partition -DiskNumber $($d.Number) -UseMaximumSize -AssignDriveLetter;
     Format-Volume -DriveLetter $($dp.DriveLetter) -NewFileSystemLabel "Service-Fabric-$i" -FileSystem NTFS;
     $i++
 }
+
+exit 0;
